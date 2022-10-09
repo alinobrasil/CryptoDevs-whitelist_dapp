af02c9dc77f17f03dcf1c769426ac99c633edadb
Ali Kim <alikim@gmail.com>
2022-10-09 12:21:48 -0400

    abi hardcode

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -2,4 +2,69 @@
 
 
 export const WHITELIST_CONTRACT_ADDRESS = "0x074BFee2f332236a5f9E5A8Ae467CE3DB4De2FA0";
-export const abi = require('../../hardhat/artifacts/contracts/Whitelist.sol/Whitelist.json').abi
+export const abi = [
+    {
+        "inputs": [
+            {
+                "internalType": "uint8",
+                "name": "_maxWhitelistedAddresses",
+                "type": "uint8"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "addAddressToWhitelist",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "maxWhitelistedAddresses",
+        "outputs": [
+            {
+                "internalType": "uint8",
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "numAddressesWhitelisted",
+        "outputs": [
+            {
+                "internalType": "uint8",
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "whitelistedAddresses",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+// require('../../hardhat/artifacts/contracts/Whitelist.sol/Whitelist.json').abi
