b7714b52d348bfc1a6bcf0a23585d48b7c2e88aa
Ali Kim <alikim@gmail.com>
2022-10-09 12:06:38 -0400

    1st commit

---
+++ my-app/constants/index.js
@@ -0,0 +1,5 @@
+
+
+
+export const WHITELIST_CONTRACT_ADDRESS = "0x074BFee2f332236a5f9E5A8Ae467CE3DB4De2FA0";
+export const abi = require('../../hardhat/artifacts/contracts/Whitelist.sol/Whitelist.json').abi
